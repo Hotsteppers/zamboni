2bba76013193bddc726ad49a0727dc66cc86457b
JohnUdacity <jackmavroudis@gmail.com>
2018-06-17 23:26:01 -0700

    add: GET /schedule from NHL API

--- zamboni/api/server.py
+++ zamboni/api/server.py
@@ -1,8 +1,19 @@
 import json
+import requests
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/healthcheck')
 def healthcheck():
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@app.route('/schedule/<startDate>/<endDate>')
+def schedule(startDate, endDate):
+    params = {'startDate': startDate, 'endDate': endDate}
+    r = requests.get('https://statsapi.web.nhl.com/api/v1/schedule', params=params)
+    json_obj = r.json()
+    return json.dumps(json_obj), 200, {'ContentType': 'application/json'}
